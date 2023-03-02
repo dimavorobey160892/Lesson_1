4046085c302fe126a92ac94ec2c0198d7e9d76b3
dimavorobey160892 <126515872+dimavorobey160892@users.noreply.github.com>
2023-03-02 21:10:31 +0200

    download files

---
+++ laboratorna 1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+char symbol = '.';
+char entersymbol;
+int sum = 0;
+do
+{
+    //entersymbol = Convert.ToChar(Console.Read());
+    entersymbol = Console.ReadKey().KeyChar;
+    if (entersymbol == ' ')
+    {
+sum++;
+    }
+}
+while (entersymbol != symbol);
+Console.WriteLine(); 
+Console.WriteLine($"Кількість пробілів {sum}");
+Console.ReadKey();
+
